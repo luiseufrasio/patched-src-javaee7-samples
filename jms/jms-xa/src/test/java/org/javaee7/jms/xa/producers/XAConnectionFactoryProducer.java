11bb985605a8328b0625be732c3f3506091f009e
Patrik Dudits <patrik.dudits@payara.fish>
2022-08-16 13:18:56 +0200

    Fix CDI annotations in JMS

--- jms/jms-xa/src/test/java/org/javaee7/jms/xa/producers/XAConnectionFactoryProducer.java
+++ jms/jms-xa/src/test/java/org/javaee7/jms/xa/producers/XAConnectionFactoryProducer.java
@@ -1,9 +1,11 @@
 package org.javaee7.jms.xa.producers;
 
 import jakarta.annotation.Resource;
+import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Produces;
 import jakarta.jms.ConnectionFactory;
 
+@Dependent
 public class XAConnectionFactoryProducer {
 
     @Produces
